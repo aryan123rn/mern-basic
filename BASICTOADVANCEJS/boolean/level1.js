396ce654b953fde1634c9b72cbc7219ba90272c7
Aryan <aryanmaharjan40@gmail.com>
2026-01-06 13:08:07 +0545

    added boolean

---
+++ BASICTOADVANCEJS/boolean/level1.js
@@ -0,0 +1,12 @@
+let a = 10
+let b = 20
+
+//check if b is greater than a
+let output = a > b
+console.log(output)
+
+//check if 10 * a and check if it is greater than or equal to b
+console.log(a * 10 >= b)
+
+//check if b is divisible by a
+console.log(b % a === 0)
