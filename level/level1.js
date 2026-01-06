8f802b86859e2af7ced346b0c00ecce10458b22e
Aryan <aryanmaharjan40@gmail.com>
2026-01-06 13:10:18 +0545

    added numbers and boolean

---
+++ level/level1.js
@@ -0,0 +1,13 @@
+let a = 2
+let b = 3
+
+//calculate sum, power, and remainder of a and b
+
+let sum = a + b;
+console.log("The sum of a and b is: " + sum);
+
+let power = a ** b;
+console.log(power);
+
+let remainder = b % a;
+console.log(remainder);
